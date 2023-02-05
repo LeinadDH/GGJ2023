8c03260bcc97de8c32bcf3b2f04eca7c897c670f
CryoStorage <iracoon2014@Gmail.com>
2023-02-05 02:23:09 -0600

    NameSpace & static function fix

--- Project Root/Assets/Developer/Code/Cryo's Code/CryoStorage.cs
+++ Project Root/Assets/Developer/Code/Cryo's Code/CryoStorage.cs
@@ -2,24 +2,23 @@ using UnityEngine;
 
 namespace CryoStorage
 {
-    public class CryoStorageMath
+    public static class CryoStorageMath
     {
-        Vector3 PointOnRadius(Vector3 center, float radius, float angle)
+        public static Vector3 PointOnRadius(Vector3 center, float radius, float angle)
         {
             float xOffset = radius * Mathf.Cos(angle);
             float zOffset = radius * Mathf.Sin(angle);
             Vector3 result = new Vector3(center.x + xOffset, center.y, center.z + zOffset);
             return result;
+            
         }
 
-        private Quaternion AimAtDirection(Vector3 center, Vector3 position)
+        public static Quaternion AimAtDirection(Vector3 center, Vector3 position)
         {
             Vector3 aimDir = center - position;
             Quaternion result = Quaternion.LookRotation(aimDir);
             return result;
         }
-        
-        
-
+  
     }
 }
