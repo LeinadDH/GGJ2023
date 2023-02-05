e13609c41bcb86371fcccae349245944a5df1570
CryoStorage <iracoon2014@Gmail.com>
2023-02-05 10:00:46 -0600

    StepsAudio

--- Project Root/Assets/Developer/Code/Leinad's Code/player/Move.cs
+++ Project Root/Assets/Developer/Code/Leinad's Code/player/Move.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    private AudioManager _manager;
     private PlayerActions _playerInput;
     private Rigidbody _rb;
     [SerializeField]private float speed = 10f;
@@ -13,10 +15,36 @@ public class Move : MonoBehaviour
     public Rigidbody Rb { get => _rb; set => _rb = value;}
     public float Speed { get => speed;set => speed = value; }
 
+    private void Start()
+    {
+        try
+        {
+            _manager = FindObjectOfType<AudioManager>();
+        }
+        catch { Debug.Log("Could not find audioManager"); }
+
+        StartCoroutine(CorWaitForAudio());
+    }
+
     public void SetComponents() 
     {
         _playerInput = new PlayerActions();
         _rb = GetComponent<Rigidbody>();
+    }
+
+    void PlayAudio()
+    {
+        if (_rb.velocity.magnitude < .3f) return;
+        _manager.PlayOneShot("Step");
+    }
+
+    IEnumerator CorWaitForAudio()
+    {
+        while (true)
+        { 
+            PlayAudio();
+            yield return new WaitForSeconds(.3f);
+        }
     }
 
     public void MovePlayer()
